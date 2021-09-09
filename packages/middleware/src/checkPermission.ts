fe562e35a8f28dce8cb30e6ddff8000d850414dd
Remie Bolte <remie@collabsoft.net>
2021-09-09 22:40:09 +0200

    fix: evaluate required session object properties

--- packages/middleware/src/checkPermission.ts
+++ packages/middleware/src/checkPermission.ts
@@ -3,25 +3,27 @@ import '@collabsoft-net/functions';
 import { SupportedPermissions } from '@collabsoft-net/enums';
 
 export const checkPermissions = async (user: Express.User, projectId?: string, ...requiredPermissions: SupportedPermissions[]): Promise<boolean> => {
-  const { uid, oauthClientId, sharedSecret, productType } = user as Session;
-  const impersonatedService = user.atlasService.as(uid, oauthClientId, sharedSecret);
   let hasAllRequiredPermissions = false;
-  if (productType === 'jira') {
-    hasAllRequiredPermissions = await impersonatedService.hasPermissions(requiredPermissions, projectId);
-  } else {
-    hasAllRequiredPermissions = await requiredPermissions.reduce(async (previous, permission) => {
-      const value = await previous;
-      if (!value) return value;
+  const { uid, oauthClientId, sharedSecret, productType } = user as Session;
+  if (uid && typeof uid === 'string' && oauthClientId && typeof oauthClientId === 'string' && sharedSecret && typeof sharedSecret === 'string') {
+    const impersonatedService = user.atlasService.as(uid, oauthClientId, sharedSecret);
+    if (productType === 'jira') {
+      hasAllRequiredPermissions = await impersonatedService.hasPermissions(requiredPermissions, projectId);
+    } else if (productType === 'confluence') {
+      hasAllRequiredPermissions = await requiredPermissions.reduce(async (previous, permission) => {
+        const value = await previous;
+        if (!value) return value;
 
-      switch (permission) {
-        case SupportedPermissions.ADMINISTER:
-          return await impersonatedService.memberOf('administrators');
-        case SupportedPermissions.SYSTEM_ADMIN:
-          return await impersonatedService.memberOf('system-administrators');
-        default:
-          return true;
-      }
-    }, Promise.resolve(true));
+        switch (permission) {
+          case SupportedPermissions.ADMINISTER:
+            return await impersonatedService.memberOf('administrators');
+          case SupportedPermissions.SYSTEM_ADMIN:
+            return await impersonatedService.memberOf('system-administrators');
+          default:
+            return true;
+        }
+      }, Promise.resolve(true));
+    }
   }
 
   return hasAllRequiredPermissions;
