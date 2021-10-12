1435e4e9dd987ea999768bbeb5fd91e0583b49b1
Remie Bolte <remie@collabsoft.net>
2021-10-12 12:01:20 +0200

    fix: export function to enable bundling

--- packages/functions/src/environment.ts
+++ packages/functions/src/environment.ts
@@ -1,19 +1,17 @@
-
-let cwd = process.cwd();
-if (cwd.indexOf('/functions') >= 0) {
-  cwd += cwd.endsWith('/') ? '../../' : '/../../';
-}
-
 import { load } from '@gdn/envify-nconf';
-load(cwd);
-
 import * as functions from 'firebase-functions';
 import { info } from 'firebase-functions/lib/logger';
 
-process.env = Object.assign({}, process.env, functions.config().env || {});
-Object.keys(process.env).forEach((key) => {
-  info(`[Environment] detected variable '${key.toUpperCase()}' from config`);
-  process.env[key.toUpperCase()] = process.env[key];
-});
+export const setEnv = (): void => {
+  let cwd = process.cwd();
+  if (cwd.indexOf('/functions') >= 0) {
+    cwd += cwd.endsWith('/') ? '../../' : '/../../';
+  }
+  load(cwd);
 
-if (!process.env.AC_BASEURL) throw new Error('Required environment variable AC_BASEURL is missing');
+  process.env = Object.assign({}, process.env, functions.config().env || {});
+  Object.keys(process.env).forEach((key) => {
+    info(`[Environment] detected variable '${key.toUpperCase()}' from config`);
+    process.env[key.toUpperCase()] = process.env[key];
+  });
+}
