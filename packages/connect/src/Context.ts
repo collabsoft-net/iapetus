aa0f9d9c86985381ba0addf354e2179b4fccdaad
Remie Bolte <remie@collabsoft.net>
2021-10-14 13:05:00 +0200

    fix: make getContext host product aware

--- packages/connect/src/Context.ts
+++ packages/connect/src/Context.ts
@@ -1,5 +1,4 @@
 import AJS from 'AJS';
-import JIRA from 'JIRA';
 
 import { findSource } from './iframe';
 
@@ -10,22 +9,70 @@ export const getContext = ({ source, data }: MessageEvent): void => {
     if (requestId) {
       frame.contentWindow.postMessage({
         requestId,
-        context: {
-          jira: {
-            issue: {
-              id: getIssueId()
-            }
-          }
-        }
+        context: generateContext()
       }, '*');
     }
   }
 };
 
+const generateContext = () => {
+  // eslint-disable-next-line
+  const JIRA = (window as any).JIRA || null;
+  // eslint-disable-next-line
+  const Confluence = (window as any).Confluence || null;
+
+  if (JIRA) {
+    return {
+      jira: {
+        issue: {
+          id: getIssueId(),
+          issueType: {
+            id: null
+          },
+          key: null
+        },
+        project: {
+          id: null,
+          key: null
+        }
+      },
+      license: {
+        active: true
+      }
+    }
+  } else if (Confluence) {
+    return {
+      confluence: {
+        content: {
+          id: null,
+          type: null,
+          version: null
+        },
+        macro: {
+          hash: null,
+          id: null,
+          outputType: null
+        },
+        space: {
+          id: null,
+          key: null
+        }
+      }
+    }
+  } else {
+    throw new Error('Could not find required objects JIRA or Confluence, unable to detect context');
+  }
+}
+
 const getIssueId = () => {
+  // eslint-disable-next-line
+  const JIRA = (window as any).JIRA || null;
+
   if (JIRA && JIRA.Issue && JIRA.Issue.getIssueId && typeof JIRA.Issue.getIssueId === 'function') {
     return JIRA.Issue.getIssueId();
-  } else {
+  } else if (AJS) {
     return AJS.Meta.get('issue-key');
+  } else {
+    return null;
   }
 };
