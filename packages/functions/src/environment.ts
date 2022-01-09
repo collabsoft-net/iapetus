ec9164db217c739d60049337610eb0ca35ad1379
Remie Bolte <remie@collabsoft.net>
2022-01-09 18:27:39 +0100

    fix: add check for required environment variables

--- packages/functions/src/environment.ts
+++ packages/functions/src/environment.ts
@@ -1,5 +1,6 @@
 import { isProduction } from '@collabsoft-net/helpers';
 import { load } from '@gdn/envify-nconf';
+import * as firebase from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import { info } from 'firebase-functions/lib/logger';
 
@@ -25,4 +26,22 @@ export const setEnv = (): void => {
     }
     process.env[key.toUpperCase()] = value;
   });
+
+  if (!process.env.FIREBASE_CONFIG) {
+    if (!process.env.FB_ADMINKEY) throw new Error(`Required environment variable FB_ADMINKEY is undefined`);
+    if (!process.env.FB_PROJECTID) throw new Error('Required environment variable FB_PROJECTID is undefined');
+    if (!process.env.FB_DATABASEURL) throw new Error('Required environment variable FB_DATABASEURL is undefined');
+  }
+
+  if (process.env.FB_ADMINKEY) {
+    info('You are running Firebase Cloud Functions using service account credentials');
+  }
+}
+
+export const getFirebaseAdminOptions = (): firebase.AppOptions|undefined => {
+  return !process.env.FIREBASE_CONFIG ? {
+    projectId: process.env.FB_PROJECTID,
+    storageBucket: process.env.FB_STORAGEBUCKET,
+    credential: process.env.FB_ADMINKEY ? firebase.credential.cert(JSON.parse(Buffer.from(process.env.FB_ADMINKEY, 'base64').toString('utf-8'))) : undefined,
+  } : undefined;
 }
