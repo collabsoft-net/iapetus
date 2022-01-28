a35af5e4a6c98ed2f6e01e9fcf12767f2d0032bf
Remie Bolte <remie@collabsoft.net>
2022-01-28 21:17:59 +0100

    fix: allow implementation class to return user object

--- packages/strategies/src/AbstractOAuthStrategy.ts
+++ packages/strategies/src/AbstractOAuthStrategy.ts
@@ -18,15 +18,15 @@ export abstract class AbstractOAuthStrategy implements Strategy {
   protected callback() {
     return async (_req: express.Request, accessToken: string, refreshToken: string, profile: unknown, done: (err: Error|null, profile?: unknown) => void): Promise<void> => {
       try {
-        await this.process(accessToken, refreshToken, profile);
-        done(null, profile);
+        const user = await this.process(accessToken, refreshToken, profile);
+        done(null, user);
       } catch (error) {
         done(error as Error);
       }
     };
   }
 
-  abstract process(accessToken: string, refreshToken: string, profile: unknown): Promise<void>;
+  abstract process<T>(accessToken: string, refreshToken: string, profile: T): Promise<T>;
 
   next(_req: express.Request, res: express.Response): void {
     res.redirect(`/`);
