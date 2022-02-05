209621d9d269094a67f2e611b7101b5b7532f2d4
Remie Bolte <remie@collabsoft.net>
2022-02-05 21:31:35 +0100

    feat: add isEmptyString helper

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -1,5 +1,6 @@
 
 export * from './camelCase';
+export * from './isEmptyString';
 export * from './isLicensingEnabled';
 export * from './isProduction';
 export * from './typeGuard';
---
+++ packages/helpers/src/isEmptyString.ts
@@ -0,0 +1,2 @@
+
+export const isEmptyString = (value: string): boolean => !(typeof value === 'string' && value.length > 0);
