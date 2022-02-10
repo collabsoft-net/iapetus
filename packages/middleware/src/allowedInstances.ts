44bc870d2a4eb23d471ac43915bb5c4f340246ec
Remie Bolte <remie@collabsoft.net>
2022-02-10 14:10:48 +0100

    fix: add support for globally allowed instances for all apps

---
+++ packages/middleware/src/allowedInstances.ts
@@ -0,0 +1,8 @@
+
+export const allowedInstances = [
+  'https://collabsoft-dev.atlassian.net',
+  'https://collabsoft-jonas.atlassian.net',
+  'https://collabsoft-acc.atlassian.net',
+  'https://remie-figma.atlassian.net',
+  'https://juliet-collabsoft.atlassian.net'
+];
--- packages/middleware/src/isAllowedInstance.ts
+++ packages/middleware/src/isAllowedInstance.ts
@@ -3,7 +3,9 @@ import { isProduction } from '@collabsoft-net/helpers';
 import * as express from 'express';
 import { warn } from 'firebase-functions/lib/logger';
 
-export const isAllowedInstance = (productionProjectId: string, allowedInstances: Array<string>) => {
+import { allowedInstances as defaultAllowedInstances } from './allowedInstances';
+
+export const isAllowedInstance = (productionProjectId: string, allowedInstances: Array<string> = defaultAllowedInstances) => {
   return (req: express.Request, res: express.Response, next: express.NextFunction): void => {
     if (!isProduction() || !process.env.FB_PROJECTID || process.env.FB_PROJECTID !== productionProjectId) {
       const { body } = req;
