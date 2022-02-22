ee86d191904ff4f54386dfe0318f810d5a08cca0
Remie Bolte <remie@collabsoft.net>
2022-02-22 10:57:55 +0100

    fix: do not use promises when registering PubSub handlers

--- packages/functions/src/registerPubSubHandlers.ts
+++ packages/functions/src/registerPubSubHandlers.ts
@@ -6,12 +6,12 @@ import * as functions from 'firebase-functions';
 import { log } from 'firebase-functions/lib/logger';
 import * as inversify from 'inversify';
 
-const localJobs: Record<string, CronJob> = {};
+const scheduledPubSubEmulatorJobs: Record<string, CronJob> = {};
 
 export const PubSubHandlers = Symbol.for('PubSubHandlers');
 export const ScheduledPubSubHandlers = Symbol.for('ScheduledPubSubHandlers');
 
-export const registerPubSubHandlers = async (module: NodeModule, container: inversify.interfaces.Container | (() => inversify.interfaces.Container)): Promise<void> => {
+export const registerPubSubHandlers = async (container: inversify.interfaces.Container | (() => inversify.interfaces.Container)): Promise<void> => {
   const appContainer = typeof container === 'function' ? container() : container;
 
   const pubSubHandlers = appContainer.isBound(PubSubHandlers) ? appContainer.getAll<PubSubHandler>(PubSubHandlers) : [];
@@ -23,18 +23,18 @@ export const registerPubSubHandlers = async (module: NodeModule, container: inve
     module.exports[name] = functions.runWith({ memory: '4GB', timeoutSeconds: 540 }).pubsub.topic(handler.topic).onPublish((message) => handler.process(message));
   });
 
-  for await (const handler of scheduledPubSubHandlers) {
+  scheduledPubSubHandlers.forEach(handler => {
     const { name, schedule } = handler;
     if (!isProduction()) {
       log(`[${name}] Registering scheduled PubSub subscription for schedule ${schedule} (using Cron)`);
-      if (!localJobs[name]) {
+      if (!scheduledPubSubEmulatorJobs[name]) {
         const job = new CronJob(schedule, () => handler.process());
-        localJobs[name] = job;
+        scheduledPubSubEmulatorJobs[name] = job;
         job.start();
       }
     } else {
       log(`[${name}] Registering scheduled PubSub subscription for schedule ${schedule} (using Google Cloud Scheduler)`);
       module.exports[name] = functions.runWith({ memory: '4GB', timeoutSeconds: 540 }).pubsub.schedule(schedule).onRun(() => handler.process());
     }
-  }
+  });
 }
