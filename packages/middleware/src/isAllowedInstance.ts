8fa42f49440744baa70b69a3ffa6ddac987c70e2
Remie Bolte <remie@collabsoft.net>
2022-02-23 19:26:18 +0100

    fix: add support for both lifecycle events and session-based requests

--- packages/middleware/src/isAllowedInstance.ts
+++ packages/middleware/src/isAllowedInstance.ts
@@ -1,17 +1,27 @@
 
-import { isProduction } from '@collabsoft-net/helpers';
+import { isNullOrEmpty, isOfType, isProduction } from '@collabsoft-net/helpers';
 import * as express from 'express';
 import { warn } from 'firebase-functions/lib/logger';
 
 import { allowedInstances as defaultAllowedInstances } from './allowedInstances';
 
+const getBaseURL = (req: express.Request): string|null => {
+  const { body, user } = req;
+  if (body && body.baseUrl) {
+    return body.baseUrl;
+  } else if (isOfType<Session>(user, 'instance')) {
+    return user.instance.baseUrl;
+  }
+  return null;
+}
+
 export const isAllowedInstance = (productionProjectId: string, allowedInstances: Array<string> = defaultAllowedInstances) => {
   return (req: express.Request, res: express.Response, next: express.NextFunction): void => {
     if (!isProduction() || !process.env.FB_PROJECTID || process.env.FB_PROJECTID !== productionProjectId) {
-      const { body } = req;
-      if (body && body.baseUrl) {
-        if (!allowedInstances.some(instance => body.baseUrl.startsWith(instance))) {
-          warn(`A non-production version is not allowed to be installed on instance ${body.baseUrl}`);
+      const baseUrl = getBaseURL(req);
+      if (!isNullOrEmpty(baseUrl)) {
+        if (!allowedInstances.some(instance => (baseUrl as string).startsWith(instance))) {
+          warn(`A non-production version is not allowed to use this endpoint (${baseUrl})`);
           res.status(401).send('Forbidden');
         } else {
           next();
@@ -24,3 +34,4 @@ export const isAllowedInstance = (productionProjectId: string, allowedInstances:
     }
   }
 }
+
