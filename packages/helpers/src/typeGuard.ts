79a122f277f1928badb6bb54d85fe8ebafe6cdb4
Remie Bolte <remie@collabsoft.net>
2022-02-24 16:35:54 +0100

    fix: add null check in typeguard

--- packages/helpers/src/typeGuard.ts
+++ packages/helpers/src/typeGuard.ts
@@ -1,7 +1,7 @@
 import { Type } from '@collabsoft-net/types';
 
 export const isOfType = <T>(obj: T | unknown, property: keyof T): obj is T => {
-    return typeof obj === 'object' && property in (obj as Record<string, unknown>);
+    return obj !== null && typeof obj === 'object' && property in (obj as Record<string, unknown>);
 };
 
 export const isTypeOf = <T, X>(instanceOrType: Type<T>|T, type: Type<X>): boolean => {
