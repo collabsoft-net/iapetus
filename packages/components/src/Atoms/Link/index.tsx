be08f299528315453b2113ca7281ebc72bfa0679
Remie Bolte <remie@collabsoft.net>
2022-05-19 15:01:50 +0200

    fix: implement review of Link atom

--- packages/components/src/Atoms/Link/index.tsx
+++ packages/components/src/Atoms/Link/index.tsx
@@ -1,9 +1,13 @@
-import Button from '@atlaskit/button';
+import Button, { Spacing } from '@atlaskit/button';
 import React from 'react';
 import { PropsWithChildren } from 'react';
 
 interface LinkProps  {
   href: string;
+  spacing?: Spacing;
+  target?: string;
 }
 
-export const Link = ({ href, children }: PropsWithChildren<LinkProps>): JSX.Element => <Button appearance='link' href={ href } spacing='none' rel='noreferrer' target='_blank'>{ children }</Button>;
+export const Link = ({ href, target, spacing, children }: PropsWithChildren<LinkProps>): JSX.Element => (
+  <Button appearance='link' href={ href } spacing={ spacing || 'none' } rel='noreferrer' target={ target || '_blank' }>{ children }</Button>
+);
