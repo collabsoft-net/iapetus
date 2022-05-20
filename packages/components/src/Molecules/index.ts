9f2e0e437dbeaa2753f6712035cdd78e2f2571ef
Remie Bolte <remie@collabsoft.net>
2022-05-20 17:48:22 +0200

    feat: addding ProjectWithAvatar and UserWithAvatar

--- packages/components/src/Molecules/index.ts
+++ packages/components/src/Molecules/index.ts
@@ -1,5 +1,7 @@
 
 export * from './Block';
 export * from './Image';
+export * from './ProjectWithAvatar';
 export * from './RecommendedApp';
 export * from './RecommendedApps';
+export * from './UserWithAvatar';
