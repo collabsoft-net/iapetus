b66086b4f19a7cd97838d9e9ad7b87203d321202
Remie Bolte <remie@collabsoft.net>
2022-05-22 10:37:11 +0200

    fix: remove host specific helpers

--- packages/types/src/ConnectHelper.ts
+++ packages/types/src/ConnectHelper.ts
@@ -1,19 +1,5 @@
 
-export interface JiraHelper {
+export interface ConnectHelper {
   getUrl: (path: string) => string;
-  getProjectId: () => number;
-  getProjectKey: () => string|undefined;
-  getServletPattern: () => string;
-}
-
-export interface ConfluenceHelper {
-  getUrl: (path: string) => string;
-  getServletPattern: () => string;
-}
-
-export interface BitbucketHelper {
-  getUrl: (path: string) => string;
-  getProjectId: () => number;
-  getProjectKey: () => string|undefined;
   getServletPattern: () => string;
 }
