54be20f51728cac360ac4ef4fa6e56838325b93d
Remie Bolte <remie@collabsoft.net>
2022-06-13 13:23:33 +0200

    feat: add the ability to create custom strategies

--- packages/strategies/src/index.ts
+++ packages/strategies/src/index.ts
@@ -1,6 +1,8 @@
 
+export * from './AbstractAtlassianCustomStrategy';
 export * from './AbstractAtlassianTokenBearerStrategy';
 export * from './AbstractAtlassianTokenJWTStrategy';
 export * from './AbstractBearerStrategy';
+export * from './AbstractCustomStrategy';
 export * from './AbstractJWTStrategy';
 export * from './AbstractOAuthStrategy';
