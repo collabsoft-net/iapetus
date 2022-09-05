d480c38ae0198e9732595cb9944eeb4f8bfab162
JonasCollabsoft <44840127+JonasCollabsoft@users.noreply.github.com>
2022-09-05 12:19:00 +0200

    Update allowedInstances.ts

--- packages/middleware/src/allowedInstances.ts
+++ packages/middleware/src/allowedInstances.ts
@@ -2,6 +2,7 @@
 export const allowedInstances = [
   'https://collabsoft-dev.atlassian.net',
   'https://collabsoft-jonas.atlassian.net',
+  'https://collabsoft-jonas-dev.atlassian.net',
   'https://collabsoft-acc.atlassian.net',
   'https://remie-figma.atlassian.net',
   'https://juliet-collabsoft.atlassian.net',
