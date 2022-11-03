50a56d809c86d2534e77c044335af02770b64e15
Remie Bolte <remie@collabsoft.net>
2022-11-03 13:48:24 +0100

    fix: add loading state and error handling to jira project provider

--- packages/components/src/Providers/JiraProjectProvider/index.tsx
+++ packages/components/src/Providers/JiraProjectProvider/index.tsx
@@ -5,32 +5,42 @@ import { ReactNode, useEffect, useState } from 'react';
 
 interface JiraProjectProviderProps {
   projectIdOrKey: string|number;
-  permissions?: Array<Jira.BulkProjectPermissions>;
+  requiredPermissions?: Array<Jira.BulkProjectPermissions>;
+  loadingMessage?: ReactNode;
   children: (args: {
     project?: Jira.Project;
     permitted?: boolean;
+    errors?: Error;
+    loading: boolean;
   }) => ReactNode;
 }
 
-export const JiraProjectProvider = ({ projectIdOrKey, permissions, children }: JiraProjectProviderProps): ReactNode => {
+export const JiraProjectProvider = ({ projectIdOrKey, requiredPermissions, loadingMessage, children }: JiraProjectProviderProps): ReactNode => {
   const [ AP ] = useState<AP.Instance>(kernel.get<AP.Instance>(ServiceIdentifier.AP));
   const [ service ] = useState(kernel.get<JiraClientService>(JiraClientService.getIdentifier()));
   const [ project, setProject ] = useState<Jira.Project>();
   const [ permitted, setPermitted ] = useState<boolean>();
+  const [ loading, setLoading ] = useState<boolean>(true);
+  const [ errors, setErrors ] = useState<Error>();
 
   useEffect(() => {
     if (AP && service) {
-      AP.user.getCurrentUser(({ atlassianAccountId }) => {
-        Promise.all([
-          service.getProject(projectIdOrKey),
-          permissions ? service.hasPermissions(atlassianAccountId, permissions) : Promise.resolve(true)
-        ]).then(([ project, permitted ]) => {
+      if (requiredPermissions) {
+        AP.user.getCurrentUser(({ atlassianAccountId }) => Promise.all([
+          service.hasPermissions(atlassianAccountId, requiredPermissions).catch(() => false),
+          service.getProject(projectIdOrKey).catch((err: Error) => { setErrors(err); return undefined })
+        ]).then(([ permitted, project ]) => {
           setProject(project);
           setPermitted(permitted);
-        });
-      });
+        }).finally(() => setLoading(false)));
+      } else {
+        service.getProject(projectIdOrKey).then(project => {
+          setProject(project);
+          setPermitted(true);
+        }).finally(() => setLoading(false));
+      }
     }
   }, [ AP, service ]);
 
-  return children({ project, permitted });
+    return loading && loadingMessage ? loadingMessage : children({ project, permitted, loading, errors });
 }
