9f95f6a4bb78a4fc954b9202d4b9503c56e29eef
Remie Bolte <remie@collabsoft.net>
2022-11-27 09:52:24 +0100

    fix: add CSS style typings and restructure code

--- packages/components/src/Atoms/Grid/index.ts
+++ packages/components/src/Atoms/Grid/index.ts
@@ -1,9 +1,65 @@
 
-import styled from 'styled-components';
+import { Property } from 'csstype';
+import styled, { ThemeProps } from 'styled-components';
+
+interface SizeProps {
+  padding?: Property.Padding;
+  margin?: Property.Margin;
+  height?: Property.Height;
+  width?: Property.Width;
+  minWidth?: Property.MinWidth;
+  maxWidth?: Property.MinHeight;
+}
+
+const getSizeProps = ({ theme: props }: ThemeProps<SizeProps>) => `
+  ${props.width && `width: ${props.width};`}
+  ${props.height && `height: ${props.height};`}
+  ${props.margin && `margin: ${props.margin};`}
+  ${props.padding && `padding: ${props.padding};`}
+  ${props.minWidth && `min-width: ${props.minWidth};`}
+  ${props.maxWidth && `max-width: ${props.maxWidth};`}
+`;
+
+interface BorderProps {
+  borderRadius?: Property.BorderRadius;
+  border?: Property.Border;
+  borderTop?: Property.BorderTop;
+  borderRight?: Property.BorderRight;
+  borderBottom?: Property.BorderBottom;
+  borderLeft?: Property.BorderLeft;
+  boxShadow?: Property.BoxShadow;
+}
+
+const getBorderProps = ({ theme: props }: ThemeProps<BorderProps>) => `
+  ${props.borderRadius && `border-radius: ${props.borderRadius};`}
+  ${props.border && `border: ${props.border};`}
+  ${props.borderTop && `border-top: ${props.borderTop};`}
+  ${props.borderRight && `border-right: ${props.borderRight};`}
+  ${props.borderBottom && `border-bottom: ${props.borderBottom};`}
+  ${props.borderLeft && `border-left: ${props.borderLeft};`}
+  ${props.boxShadow && `box-shadow: ${props.boxShadow};`}
+`;
+
+interface FlexProps {
+  gap?: Property.Gap;
+  justifyContent?: Property.JustifyContent;
+  alignItems?: Property.AlignItems;
+  align?: Property.AlignSelf;
+  wraps?: boolean; // 'wrap' upsets React.DOM
+}
+
+const getFlexProps = ({ theme: props }: ThemeProps<FlexProps>) => `
+  ${props.gap && `gap: ${props.gap};`}
+  ${props.justifyContent && `justify-content: ${props.justifyContent};`}
+  ${props.alignItems && `align-items: ${props.alignItems};`}
+  ${props.align && `align-self: ${props.align};`}
+  ${props.wraps && `flex-wrap: wrap;`}
+`;
+
 
 interface PageProps {
-  background?: string;
-  padding?: string;
+  background?: Property.Background;
+  padding?: Property.Padding;
 }
 
 export const Page = styled.div<PageProps>`
@@ -13,22 +69,12 @@ export const Page = styled.div<PageProps>`
   width: 100%;
 `;
 
-export interface GridProps {
-  vertical?: boolean;
-  padding?: string;
-  margin?: string;
+export interface GridProps extends SizeProps, BorderProps, FlexProps {
   fluid?: boolean;
-  width?: string;
-  height?: string;
+  vertical?: boolean;
   stretched?: boolean;
-  background?: string;
-  boxShadow?: string;
-  wraps?: boolean; // 'wrap' upsets React.DOM
-  gap?: string;
-  justifyContent?: string;
-  alignItems?: string;
-  borderRadius?: string;
-  border?: string;
+  background?: Property.Background;
+  cursor?: Property.Cursor;
 }
 
 export const Grid = styled.div<GridProps>`
@@ -36,21 +82,15 @@ export const Grid = styled.div<GridProps>`
   flex-direction: ${(props: GridProps) => !props.vertical ? 'column' : 'row' };
   box-sizing: border-box;
 
-  ${props => props.margin && `margin: ${props.margin};`}
-  ${props => props.padding && `padding: ${props.padding};`}
+  ${props => getSizeProps(props)}
+  ${props => getBorderProps(props)}
+  ${props => getFlexProps(props)}
+
   ${props => props.vertical && !props.padding && `height: 100%;`}
   ${props => !props.vertical && !props.padding && `width: ${props.width || '100%'};`}
-  ${props => props.width && `width: ${props.width};`}
-  ${props => props.height && `height: ${props.height};`}
   ${props => props.stretched && `width: 100%; height: 100%;`}
   ${props => props.background && `background: ${props.background};`}
-  ${props => props.wraps && `flex-wrap: wrap;`}
-  ${props => props.justifyContent && `justify-content: ${props.justifyContent};`}
-  ${props => props.alignItems && `align-items: ${props.alignItems};`}
-  ${props => props.gap && `gap: ${props.gap};`}
-  ${props => props.border && `border: ${props.border};`}
-  ${props => props.borderRadius && `border-radius: ${props.borderRadius};`}
-  ${props => props.boxShadow && `box-shadow: ${props.boxShadow};`}
+  ${props => props.cursor && `cursor: ${props.cursor};`}
 
   ${props => !props.fluid && !props.stretched && `
     @media(min-width: 990px) {
@@ -62,50 +102,26 @@ export const Grid = styled.div<GridProps>`
   --component: grid;
 `;
 
-export interface ColumnProps {
-  margin?: string;
-  padding?: string;
+export interface ColumnProps extends SizeProps, BorderProps, FlexProps {
   span?: string|number;
   stretched?: boolean;
-  align?: string;
-  height?: string;
-  width?: string;
-  minWidth?: string;
-  maxWidth?: string;
-  borderRadius?: string;
-  border?: string;
-  borderTop?: string;
-  borderRight?: string;
-  borderBottom?: string;
-  borderLeft?: string;
-  background?: string;
-  boxShadow?: string;
+  background?: Property.Background;
+  cursor?: Property.Cursor;
 }
 
 export const Column = styled.div<ColumnProps>`
-  ${props => props.margin && `margin: ${props.margin};`}
-  ${props => props.padding && `padding: ${props.padding};`}
-  ${props => props.align && `align-self: ${props.align};`}
+  ${props => getSizeProps(props)}
+  ${props => getBorderProps(props)}
+  ${props => getFlexProps(props)}
+
   ${props => !props.stretched && `flex: ${props.span ? props.span : '0 0 auto'};`}
   ${props => props.stretched && `flex: 1 1 auto; overflow: auto;`}
   ${props => props.stretched && !props.width && `width: 100%;`}
   ${props => props.stretched && !props.height && `height: 100%;`}
-  ${props => props.height && `height: ${props.height};`}
-  ${props => props.width && `width: ${props.width};`}
-  ${props => props.minWidth && `min-width: ${props.minWidth};`}
-  ${props => props.maxWidth && `max-width: ${props.maxWidth};`}
-  ${props => props.border && `border: ${props.border};`}
-  ${props => props.borderTop && `border-top: ${props.borderTop};`}
-  ${props => props.borderRight && `border-right: ${props.borderRight};`}
-  ${props => props.borderBottom && `border-bottom: ${props.borderBottom};`}
-  ${props => props.borderLeft && `border-left: ${props.borderLeft};`}
-  ${props => props.borderRadius && `border-radius: ${props.borderRadius};`}
   ${props => props.background && `background: ${props.background};`}
-  ${props => props.boxShadow && `box-shadow: ${props.boxShadow};`}
+
   box-sizing: border-box;
   --component: column;
 `;
 
 export const Row = styled(Column)`--component: row;`;
-
-
