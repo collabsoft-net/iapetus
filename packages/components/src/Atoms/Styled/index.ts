b347529b7ff7fd2809e0e3148e849f3cf17708b8
Remie Bolte <remie@collabsoft.net>
2023-04-11 12:50:40 +0200

    fix: stop whining about something that already worked

--- packages/components/src/Atoms/Styled/index.ts
+++ packages/components/src/Atoms/Styled/index.ts
@@ -5,7 +5,7 @@ import { ThemedCssFunction, ThemedStyledFunction } from 'styled-components';
 export const withProps = <U>() =>
     <P, T, O>( // eslint-disable-line
         fn: ThemedStyledFunction<P, T, O>|ThemedCssFunction<P>
-    ) => fn as ThemedStyledFunction<P & U, T, O & U>;
+    ) => fn as unknown as ThemedStyledFunction<P & U, T, O & U>;
 
 export interface SizeProps {
     padding?: Property.Padding;
