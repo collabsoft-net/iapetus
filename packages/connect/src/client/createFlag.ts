6b9ce1f5e2be29a536a71fe84b7f77394a04b275
Remie Bolte <remie@collabsoft.net>
2023-05-16 13:51:02 +0200

    feat: add ability to close flags based on their internal ID

--- packages/connect/src/client/createFlag.ts
+++ packages/connect/src/client/createFlag.ts
@@ -2,6 +2,8 @@ import kernel from '@collabsoft-net/inversify';
 
 import { ServiceIdentifier } from '../ServiceIdentifier';
 
+const flags = new Map<string, AP.FlagInstance>();
+
 export function showFlag(title: string): AP.FlagInstance;
 export function showFlag(title: string, type: AP.FlagTypeOptions): AP.FlagInstance;
 export function showFlag(title: string, type: AP.FlagTypeOptions, body: string): AP.FlagInstance;
@@ -15,8 +17,19 @@ export function showFlag(titleOrOptions: string|AP.FlagOptions, type?: AP.FlagTy
       type: type || 'info',
       close: 'auto'
     };
-    return AP.flag.create(flagOptions);
+
+    const flag = AP.flag.create(flagOptions);
+    flags.set(flag._id, flag);
+    return flag;
   } else {
     throw new Error(`Failed to retrieve required object 'AP' using service identifier '${String(ServiceIdentifier.AP)}'`);
+  }
+}
+
+export function closeFlag(flagId: string) {
+  const flag = flags.get(flagId);
+  if (flag) {
+    flag.close();
+    flags.delete(flagId);
   }
 }
