113f21f7f7ee050469851def3f83bd5ab5e5d760
Remie Bolte <remie@collabsoft.net>
2023-05-23 11:00:05 +0200

    fix: make isNullOrEmpty type-aware

--- packages/helpers/src/isNullOrEmpty.ts
+++ packages/helpers/src/isNullOrEmpty.ts
@@ -1,8 +1,11 @@
 
-export const isNullOrEmpty = (value: string | string[] | undefined | null): boolean => {
+export const isNullOrEmpty = <T> (value: T | undefined | null): value is Exclude<typeof value, T> => {
   if (value === undefined || value === null) {
-    return true;
-  } else {
-    return value.length <= 0;
+    return false;
+  } else if (typeof value === 'string' || Array.isArray(value)) {
+    return value.length > 0;
+  } else if (typeof value === 'object') {
+    return Object.keys(value).length > 0;
   }
+  return true;
 }
