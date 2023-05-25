90e7157ec8319e89a85251b11b9f52311ab247a7
Remie Bolte <remie@collabsoft.net>
2023-05-25 22:19:43 +0200

    feat: add support for `editor` module type

--- packages/connect/src/client/isValidConnectRequest.ts
+++ packages/connect/src/client/isValidConnectRequest.ts
@@ -4,13 +4,14 @@ export const isValidConnectRequest = () => {
     const query = new URLSearchParams(window.location.search);
     const pageId = query.get('p');
     const dialogId = query.get('d');
+    const editorId = query.get('e');
 
     // Also keep legacy moduleId (s) for backwards compatibility
     const moduleId = query.get('s');
 
     return {
-      moduleId: moduleId || pageId || dialogId,
-      moduleType: pageId ? 'page' : dialogId ? 'dialog' : 'legacy'
+      moduleId: moduleId || pageId || dialogId || editorId,
+      moduleType: pageId ? 'page' : dialogId ? 'dialog' : editorId ? 'editor' : 'legacy'
     }
   }
   return null;
