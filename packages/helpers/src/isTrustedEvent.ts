0e35929f13bfe156432b21afc6639370ac71e4b8
Remie Bolte <remie@collabsoft.net>
2023-05-26 14:01:02 +0200

    fix: also support native events

--- packages/helpers/src/isTrustedEvent.ts
+++ packages/helpers/src/isTrustedEvent.ts
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export const isTrustedEvent = <T extends React.SyntheticEvent> (handler?: React.EventHandler<T>) => {
+export function isTrustedEvent<T extends MouseEvent>(handler?: (event: T) => void): (event: T) => void;
+export function isTrustedEvent<T extends React.SyntheticEvent>(handler?: React.EventHandler<T>): (event: T) => void;
+export function isTrustedEvent<T extends React.SyntheticEvent|MouseEvent>(handler?: (event: T) => void): (event: T) => void {
   return (event: T) => {
     if (event.isTrusted) {
       handler && handler(event);
     }
   }
 }
+
