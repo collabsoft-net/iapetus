d11281f7f330fb55db6c0ccb5ad0818c0ab8452e
Remie Bolte <remie@collabsoft.net>
2023-07-16 11:10:19 +0200

    fix: RestClientService should extend AbstractRestClientService

--- packages/components/src/Contexts/RestClientService/index.ts
+++ packages/components/src/Contexts/RestClientService/index.ts
@@ -1,4 +1,4 @@
 import { AbstractRestClientService } from '@collabsoft-net/services';
 import { createContext } from 'react';
 
-export const RestClientService = createContext<AbstractRestClientService|null>(null);
+export const RestClientService = createContext<typeof AbstractRestClientService|null>(null);
