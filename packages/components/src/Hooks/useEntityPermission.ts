8509058bf1825b478d1bb874391aeaf24a06cf9d
Remie Bolte <remie@collabsoft.net>
2023-07-17 08:03:01 +0200

    fix: make choice between content or space permissions explicit

--- packages/components/src/Hooks/useEntityPermission.ts
+++ packages/components/src/Hooks/useEntityPermission.ts
@@ -7,15 +7,15 @@ import { ConfluenceClientService, JiraClientService } from '../Contexts';
 import { useCurrentUser } from './useCurrentUser';
 import { useHostContext } from './useHostContext';
 
-export function usePermission(permissions: Array<string>, issueId?: string, accountId?: string): [ boolean|undefined, boolean ];
-export function usePermission(permissions: Confluence.ContentOperation, contentId?: string, accountId?: string): [ boolean|undefined, boolean ];
-export function usePermission(permissions: Confluence.ContentOperation, spaceKey?: string, accountId?: string): [ boolean|undefined, boolean ];
-export function usePermission(permissions: Array<string>|Confluence.ContentOperation, issueIdOrContentIdOrSpaceKey?: string, accountId?: string): [ boolean|undefined, boolean ] {
+export function usePermission(permissions: Array<string>, issueId?: number, accountId?: string): [ boolean|undefined, boolean ];
+export function usePermission(permissions: Confluence.ContentOperation, type: 'content', contentId?: string, accountId?: string): [ boolean|undefined, boolean ];
+export function usePermission(permissions: Confluence.ContentOperation, type: 'space', spaceKey?: string, accountId?: string): [ boolean|undefined, boolean ];
+export function usePermission(permissions: Array<string>|Confluence.ContentOperation, issueIdOrType?: number|'content'|'space', accountIdOrContentIdOrSpaceKey?: string, accountId?: string): [ boolean|undefined, boolean ] {
 
   const jiraService = useContext(JiraClientService);
   const confluenceService = useContext(ConfluenceClientService);
 
-  const [ user ] = useCurrentUser(accountId);
+  const [ user ] = useCurrentUser(typeof issueIdOrType === 'number' ? accountIdOrContentIdOrSpaceKey : accountId);
   const [ context ] = useHostContext();
 
   const [ isLoading, setLoading ] = useState<boolean>(true);
@@ -23,8 +23,8 @@ export function usePermission(permissions: Array<string>|Confluence.ContentOpera
 
   useEffect(() => {
     if (context && user) {
-      if (isOfType<AP.JiraContext>(context, 'jira') && Array.isArray(permissions)) {
-        const entityId = issueIdOrContentIdOrSpaceKey || context.jira.issue.id;
+      if (isOfType<AP.JiraContext>(context, 'jira') && Array.isArray(permissions) && typeof issueIdOrType === 'number') {
+        const entityId = issueIdOrType || context.jira.issue.id;
         if (jiraService && entityId) {
           jiraService.hasPermissions(user.accountId, [
             {
@@ -36,17 +36,26 @@ export function usePermission(permissions: Array<string>|Confluence.ContentOpera
           setHasPermissions(undefined);
           setLoading(false);
         }
-      } else if (isOfType<AP.ConfluenceContext>(context, 'confluence') && !Array.isArray(permissions)) {
-        const contentId = issueIdOrContentIdOrSpaceKey || context.confluence.content.id;
-        const spaceKey = issueIdOrContentIdOrSpaceKey || context.confluence.space.key;
+      } else if (isOfType<AP.ConfluenceContext>(context, 'confluence') && !Array.isArray(permissions) && issueIdOrType === 'content') {
+        const contentId = accountIdOrContentIdOrSpaceKey || context.confluence.content.id;
 
-        if (confluenceService) {
+        if (confluenceService && contentId) {
           confluenceService.hasContentPermission(contentId, { type: 'user', identifier: user.accountId }, permissions)
             .then(setHasPermissions)
-            .catch(() => confluenceService.hasSpacePermission(spaceKey, permissions)
-                .then(setHasPermissions)
-                .catch(() => setHasPermissions(false))
-            ).finally(() => setLoading(false));
+            .catch(() => setHasPermissions(false))
+            .finally(() => setLoading(false));
+        } else {
+          setHasPermissions(undefined);
+          setLoading(false);
+        }
+      } else if (isOfType<AP.ConfluenceContext>(context, 'confluence') && !Array.isArray(permissions) && issueIdOrType === 'space') {
+        const spaceKey = accountIdOrContentIdOrSpaceKey || context.confluence.space.key;
+
+        if (confluenceService && spaceKey) {
+          confluenceService.hasSpacePermission(spaceKey, permissions)
+            .then(setHasPermissions)
+            .catch(() => setHasPermissions(false))
+            .finally(() => setLoading(false));
         } else {
           setHasPermissions(undefined);
           setLoading(false);
