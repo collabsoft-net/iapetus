3889010c40288ed40cb1718170bc65c54ee3760c
Remie Bolte <remie@collabsoft.net>
2023-07-18 21:51:58 +0200

    fix: do not accept null value for contexts

--- packages/components/src/Contexts/AP/index.ts
+++ packages/components/src/Contexts/AP/index.ts
@@ -5,10 +5,8 @@ import React from 'react';
 
 const windowWithAP = window as unknown as Window & { AP: AP.Instance };
 
-export const AP = React.createContext<AP.Instance|null>(
+export const AP = React.createContext<AP.Instance>(
   windowWithAP.AP
     ? windowWithAP.AP
-    : kernel.isBound(ServiceIdentifier.AP)
-      ? kernel.get<AP.Instance>(ServiceIdentifier.AP)
-      : null
+    : kernel.get<AP.Instance>(ServiceIdentifier.AP)
 );
--- packages/components/src/Contexts/JiraClientService/index.ts
+++ packages/components/src/Contexts/JiraClientService/index.ts
@@ -3,8 +3,4 @@ import kernel from '@collabsoft-net/inversify';
 import { JiraClientService as Service } from '@collabsoft-net/services';
 import React from 'react';
 
-export const JiraClientService = React.createContext<Service|null>(
-  kernel.isBound(Service.getIdentifier())
-    ? kernel.get<Service>(Service.getIdentifier())
-    : null
-);
+export const JiraClientService = React.createContext<Service>(kernel.get<Service>(Service.getIdentifier()));
