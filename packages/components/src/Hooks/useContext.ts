4667a19ab6092ee6125e530804e86f1619487592
Remie Bolte <remie@collabsoft.net>
2023-09-12 19:56:14 +0200

    feat: add useContext() hook

--- packages/components/src/Hooks/useContext.ts
+++ packages/components/src/Hooks/useContext.ts
@@ -1,19 +1,15 @@
 
 import kernel from '@collabsoft-net/inversify';
+import { interfaces } from 'inversify';
 import React from 'react';
 
-export const useContext = <T> (context: React.Context<T>, serviceIdentifier?: string) => {
-
+export const useContext = <T> (context: React.Context<T>, serviceIdentifier?: interfaces.ServiceIdentifier<T>) => {
   const ctx = React.useContext(context);
-
   if (ctx) {
     return ctx;
-  } else if (serviceIdentifier) {
-    const fallback = kernel.get<T>(serviceIdentifier);
-    if (fallback) {
-      return fallback;
-    }
+  } else if (serviceIdentifier && kernel.isBound(serviceIdentifier)) {
+    return kernel.get<T>(serviceIdentifier);
+  } else {
+    throw new Error(`Failed to retrieve context, either through React.useContext() or IoC containter with service identifier '${serviceIdentifier?.toString()}'`);
   }
-
-  throw new Error(`Failed to retrieve context, either through React.useContext() or IoC containter with service identifier '${serviceIdentifier}'`);
 }
