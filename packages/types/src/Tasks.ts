9fb88235467d569a565dd4878a1bfcffc0c770cd
Remie Bolte <remie@collabsoft.net>
2023-10-13 17:24:38 +0200

    fix: add missing `options` property

--- packages/types/src/Tasks.ts
+++ packages/types/src/Tasks.ts
@@ -1,6 +1,9 @@
+import { TaskQueueOptions } from 'firebase-functions/v2/tasks';
+
 import { CustomEvent, TenantAwareEvent } from './Events';
 
 export interface TaskHandler<T extends TenantAwareEvent> {
   name: string;
+  options?: TaskQueueOptions;
   process: (event: CustomEvent<T>) => Promise<void>;
 }
