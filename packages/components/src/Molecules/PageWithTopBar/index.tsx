ae77c2edd59fb42875b56480bb76edf425e23894
Remie Bolte <remie@collabsoft.net>
2024-03-17 07:42:02 +0100

    feat: add PageWithTopBar component

---
+++ packages/components/src/Molecules/PageWithTopBar/index.tsx
@@ -0,0 +1,37 @@
+import { colors } from '@atlaskit/theme';
+import { token } from '@atlaskit/tokens';
+import { Grid, Page, Row } from '@collabsoft-net/components';
+import React, { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+
+import { TopBar } from '../../Atoms/TopBar';
+
+const FullPage = styled(Page)`
+  display: flex;
+  overflow: hidden;
+`;
+
+const ScrollView = styled(Grid)`
+  overflow: scroll;
+`;
+
+interface PageWithNavBarProps {
+  title: string;
+  Icon: JSX.Element;
+  breadCrumbs?: Array<JSX.Element>;
+}
+
+export const PageWithTopBar = ({ title, Icon, breadCrumbs, children }: PropsWithChildren<PageWithNavBarProps>) => (
+  <FullPage height='100%' background={ token('elevation.surface.sunken', colors.N400A) }>
+    <TopBar title={ title } Icon={ Icon } breadCrumbs={ breadCrumbs } />
+    <ScrollView fluid height='unset' margin='60px 0 0'>
+      <Row>
+        <Grid width='768px' margin='0 auto' padding='24px 0'>
+          <Row>
+            {children}
+          </Row>
+        </Grid>
+      </Row>
+    </ScrollView>
+  </FullPage>
+)
--- packages/components/src/Molecules/index.ts
+++ packages/components/src/Molecules/index.ts
@@ -1,6 +1,7 @@
 
 export * from './Block';
 export * from './IconWithLabel';
+export * from './PageWithTopBar';
 export * from './ProjectWithAvatar';
 export * from './RecommendedApp';
 export * from './RecommendedApps';
