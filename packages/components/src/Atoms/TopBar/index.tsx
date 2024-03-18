04df45c1d83c95d0c16e86985beba5634d48b388
Remie Bolte <remie@collabsoft.net>
2024-03-18 21:24:58 +0100

    fix: Confluence pages with top bar should look different

--- packages/components/src/Atoms/TopBar/index.tsx
+++ packages/components/src/Atoms/TopBar/index.tsx
@@ -1,12 +1,17 @@
 import ChevronRight from '@atlaskit/icon/glyph/chevron-right';
 import { colors } from '@atlaskit/theme';
 import { token } from '@atlaskit/tokens';
+import { ServiceIdentifier } from '@collabsoft-net/connect';
 import { isOfType } from '@collabsoft-net/helpers';
+import kernel from '@collabsoft-net/inversify';
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
 
+import { AP } from '../../Contexts/AP';
+import { useContext } from '../../Hooks';
 import { IconWithLabel } from '../../Molecules/IconWithLabel';
 import { Column, Grid, Row } from '../Grid';
+import { withProps } from '../Styled';
 import { Header } from '../Typography';
 
 interface TopBarProps {
@@ -15,37 +20,45 @@ interface TopBarProps {
   breadCrumbs?: Array<string|JSX.Element>;
 }
 
-const FixedTopbar = styled(Grid)`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 1000;
+const Topbar = withProps<{ isFixed: boolean }>()(styled(Grid))`
+  ${props => props.isFixed && `
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 1000;
+  `}
 `;
 
-export const TopBar = ({ title, Icon, breadCrumbs }: TopBarProps) => (
-  <FixedTopbar borderBottom={ `1px solid ${token('color.border', colors.N20)}` } background={ token('elevation.surface', colors.N0) } fluid>
-    <Row borderBottom={ `border-bottom: 1px solid ${colors.N30}; `} padding='16px'>
-      <Grid fluid vertical alignItems='center'>
-        { Icon && (
-          <Column margin="0 8px 0 0">
-            <IconWithLabel src={ Icon } />
+export const TopBar = ({ title, Icon, breadCrumbs }: TopBarProps) => {
+
+  const ACJS = useContext(AP, kernel.get(ServiceIdentifier.AP));
+  const isConfluence = isOfType<AP.ConfluenceInstance>(ACJS, 'confluence');
+
+  return (
+    <Topbar isFixed={ !isConfluence } borderBottom={ !isConfluence ? `1px solid ${token('color.border', colors.N20)}` : undefined } background={ token('elevation.surface', colors.N0) } fluid={ !isConfluence }>
+      <Row padding='16px'>
+        <Grid fluid vertical alignItems='center'>
+          { Icon && (
+            <Column margin="0 8px 0 0">
+              <IconWithLabel src={ Icon } />
+            </Column>
+          )}
+          <Column align='center' margin='0 8px 0 0'>
+            <Header weight='h400'>{ title }</Header>
           </Column>
-        )}
-        <Column align='center' margin='0 8px 0 0'>
-          <Header weight='h400'>{ title }</Header>
-        </Column>
-        { breadCrumbs?.map(element => (
-          <Fragment key={ isOfType<JSX.Element>(element, 'key') ? element.key : element }>
-            <Column align='center' margin='0 8px 0 0'>
-              <IconWithLabel src={ <ChevronRight label='Breadcrumb' /> } />
-            </Column>
-            <Column align='center' cursor='pointer'>
-              { element }
-            </Column>
-          </Fragment>
-        ))}
-      </Grid>
-    </Row>
-  </FixedTopbar>
-);
+          { breadCrumbs?.map(element => (
+            <Fragment key={ isOfType<JSX.Element>(element, 'key') ? element.key : element }>
+              <Column align='center' margin='0 8px 0 0'>
+                <IconWithLabel src={ <ChevronRight label='Breadcrumb' /> } />
+              </Column>
+              <Column align='center' cursor='pointer'>
+                { element }
+              </Column>
+            </Fragment>
+          ))}
+        </Grid>
+      </Row>
+    </Topbar>
+  );
+}
--- packages/components/src/Molecules/PageWithTopBar/index.tsx
+++ packages/components/src/Molecules/PageWithTopBar/index.tsx
@@ -1,15 +1,21 @@
 import { colors } from '@atlaskit/theme';
 import { token } from '@atlaskit/tokens';
+import { ServiceIdentifier } from '@collabsoft-net/connect';
+import { isOfType } from '@collabsoft-net/helpers';
+import kernel from '@collabsoft-net/inversify';
 import React, { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
 import { Grid, Row } from '../../Atoms/Grid';
 import { Page } from '../../Atoms/Page';
 import { TopBar } from '../../Atoms/TopBar';
+import { AP } from '../../Contexts/AP';
+import { useContext } from '../../Hooks';
 
 const FullPage = styled(Page)`
   display: flex;
   overflow: hidden;
+  flex-direction: column;
 `;
 
 const ScrollView = styled(Grid)`
@@ -22,17 +28,23 @@ interface PageWithNavBarProps {
   breadCrumbs?: Array<string|JSX.Element>;
 }
 
-export const PageWithTopBar = ({ title, Icon, breadCrumbs, children }: PropsWithChildren<PageWithNavBarProps>) => (
-  <FullPage height='100%' background={ token('elevation.surface.sunken', colors.N400A) }>
-    <TopBar title={ title } Icon={ Icon } breadCrumbs={ breadCrumbs } />
-    <ScrollView fluid height='unset' margin='60px 0 0'>
-      <Row>
-        <Grid margin='0 auto' padding='24px 0'>
-          <Row>
-            {children}
-          </Row>
-        </Grid>
-      </Row>
-    </ScrollView>
-  </FullPage>
-)
+export const PageWithTopBar = ({ title, Icon, breadCrumbs, children }: PropsWithChildren<PageWithNavBarProps>) => {
+
+  const ACJS = useContext(AP, kernel.get(ServiceIdentifier.AP));
+  const isConfluence = isOfType<AP.ConfluenceInstance>(ACJS, 'confluence');
+
+  return (
+    <FullPage height={ !isConfluence ? '100%' : undefined } background={ token(isConfluence ? 'elevation.surface' : 'elevation.surface.sunken', isConfluence ? colors.N0 : colors.N400A) }>
+      <TopBar title={ title } Icon={ Icon } breadCrumbs={ breadCrumbs } />
+      <ScrollView fluid height='unset' margin={ !isConfluence ? '60px 0 0' : undefined }>
+        <Row>
+          <Grid margin='0 auto' padding={ isConfluence ? '16px 0' : '24px 0' }>
+            <Row>
+              {children}
+            </Row>
+          </Grid>
+        </Row>
+      </ScrollView>
+    </FullPage>
+  )
+}
