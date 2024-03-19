06ae68cb87186f1ec1027850c16f1274542a4308
Remie Bolte <remie@collabsoft.net>
2024-03-19 06:24:58 +0100

    fix: make it more generic and not product specific

--- packages/components/src/Atoms/TopBar/index.tsx
+++ packages/components/src/Atoms/TopBar/index.tsx
@@ -1,14 +1,10 @@
 import ChevronRight from '@atlaskit/icon/glyph/chevron-right';
 import { colors } from '@atlaskit/theme';
 import { token } from '@atlaskit/tokens';
-import { ServiceIdentifier } from '@collabsoft-net/connect';
 import { isOfType } from '@collabsoft-net/helpers';
-import kernel from '@collabsoft-net/inversify';
 import React, { Fragment } from 'react';
 import styled from 'styled-components';
 
-import { AP } from '../../Contexts/AP';
-import { useContext } from '../../Hooks';
 import { IconWithLabel } from '../../Molecules/IconWithLabel';
 import { Column, Grid, Row } from '../Grid';
 import { withProps } from '../Styled';
@@ -18,10 +14,11 @@ interface TopBarProps {
   title: string;
   Icon: JSX.Element;
   breadCrumbs?: Array<string|JSX.Element>;
+  fixedOnTop?: boolean;
 }
 
-const Topbar = withProps<{ isFixed: boolean }>()(styled(Grid))`
-  ${props => props.isFixed && `
+const Topbar = withProps<{ fixedOnTop?: boolean }>()(styled(Grid))`
+  ${props => props.fixedOnTop && `
     position: fixed;
     top: 0;
     left: 0;
@@ -30,35 +27,29 @@ const Topbar = withProps<{ isFixed: boolean }>()(styled(Grid))`
   `}
 `;
 
-export const TopBar = ({ title, Icon, breadCrumbs }: TopBarProps) => {
-
-  const ACJS = useContext(AP, kernel.get(ServiceIdentifier.AP));
-  const isConfluence = isOfType<AP.ConfluenceInstance>(ACJS, 'confluence');
-
-  return (
-    <Topbar isFixed={ !isConfluence } borderBottom={ !isConfluence ? `1px solid ${token('color.border', colors.N20)}` : undefined } background={ token('elevation.surface', colors.N0) } fluid={ !isConfluence }>
-      <Row padding='16px'>
-        <Grid fluid vertical alignItems='center'>
-          { Icon && (
-            <Column margin="0 8px 0 0">
-              <IconWithLabel src={ Icon } />
-            </Column>
-          )}
-          <Column align='center' margin='0 8px 0 0'>
-            <Header weight='h400'>{ title }</Header>
+export const TopBar = ({ title, Icon, breadCrumbs, fixedOnTop }: TopBarProps) => (
+  <Topbar fixedOnTop={ fixedOnTop } borderBottom={ fixedOnTop ? `1px solid ${token('color.border', colors.N20)}` : undefined } background={ token('elevation.surface', colors.N0) } fluid={ fixedOnTop }>
+    <Row padding='16px'>
+      <Grid fluid vertical alignItems='center'>
+        { Icon && (
+          <Column margin="0 8px 0 0">
+            <IconWithLabel src={ Icon } />
           </Column>
-          { breadCrumbs?.map(element => (
-            <Fragment key={ isOfType<JSX.Element>(element, 'key') ? element.key : element }>
-              <Column align='center' margin='0 8px 0 0'>
-                <IconWithLabel src={ <ChevronRight label='Breadcrumb' /> } />
-              </Column>
-              <Column align='center' cursor='pointer'>
-                { element }
-              </Column>
-            </Fragment>
-          ))}
-        </Grid>
-      </Row>
-    </Topbar>
-  );
-}
+        )}
+        <Column align='center' margin='0 8px 0 0'>
+          <Header weight='h400'>{ title }</Header>
+        </Column>
+        { breadCrumbs?.map(element => (
+          <Fragment key={ isOfType<JSX.Element>(element, 'key') ? element.key : element }>
+            <Column align='center' margin='0 8px 0 0'>
+              <IconWithLabel src={ <ChevronRight label='Breadcrumb' /> } />
+            </Column>
+            <Column align='center' cursor='pointer'>
+              { element }
+            </Column>
+          </Fragment>
+        ))}
+      </Grid>
+    </Row>
+  </Topbar>
+);
--- packages/components/src/Molecules/PageWithTopBar/index.tsx
+++ packages/components/src/Molecules/PageWithTopBar/index.tsx
@@ -1,16 +1,11 @@
 import { colors } from '@atlaskit/theme';
 import { token } from '@atlaskit/tokens';
-import { ServiceIdentifier } from '@collabsoft-net/connect';
-import { isOfType } from '@collabsoft-net/helpers';
-import kernel from '@collabsoft-net/inversify';
 import React, { PropsWithChildren } from 'react';
 import styled from 'styled-components';
 
 import { Grid, Row } from '../../Atoms/Grid';
 import { Page } from '../../Atoms/Page';
 import { TopBar } from '../../Atoms/TopBar';
-import { AP } from '../../Contexts/AP';
-import { useContext } from '../../Hooks';
 
 const FullPage = styled(Page)`
   display: flex;
@@ -26,25 +21,20 @@ interface PageWithNavBarProps {
   title: string;
   Icon: JSX.Element;
   breadCrumbs?: Array<string|JSX.Element>;
+  inline?: boolean;
 }
 
-export const PageWithTopBar = ({ title, Icon, breadCrumbs, children }: PropsWithChildren<PageWithNavBarProps>) => {
-
-  const ACJS = useContext(AP, kernel.get(ServiceIdentifier.AP));
-  const isConfluence = isOfType<AP.ConfluenceInstance>(ACJS, 'confluence');
-
-  return (
-    <FullPage height={ !isConfluence ? '100%' : undefined } background={ token(isConfluence ? 'elevation.surface' : 'elevation.surface.sunken', isConfluence ? colors.N0 : colors.N400A) }>
-      <TopBar title={ title } Icon={ Icon } breadCrumbs={ breadCrumbs } />
-      <ScrollView fluid height='unset' margin={ !isConfluence ? '60px 0 0' : undefined }>
-        <Row>
-          <Grid margin='0 auto' padding={ isConfluence ? '16px 0' : '24px 0' }>
-            <Row>
-              {children}
-            </Row>
-          </Grid>
-        </Row>
-      </ScrollView>
-    </FullPage>
-  )
-}
+export const PageWithTopBar = ({ title, Icon, breadCrumbs, inline, children }: PropsWithChildren<PageWithNavBarProps>) => (
+  <FullPage height={ !inline ? '100%' : undefined } background={ token(inline ? 'elevation.surface' : 'elevation.surface.sunken', inline ? colors.N0 : colors.N400A) }>
+    <TopBar title={ title } Icon={ Icon } breadCrumbs={ breadCrumbs } fixedOnTop={ !inline } />
+    <ScrollView fluid height='unset' margin={ !inline ? '60px 0 0' : undefined }>
+      <Row>
+        <Grid margin='0 auto' padding={ inline ? '16px 0' : '24px 0' }>
+          <Row>
+            {children}
+          </Row>
+        </Grid>
+      </Row>
+    </ScrollView>
+  </FullPage>
+)
