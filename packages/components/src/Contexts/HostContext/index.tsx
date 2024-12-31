0275147cca0d4c2e5686c9e8dbb5c5820d431639
Remie Bolte <remie@collabsoft.net>
2024-12-31 09:25:59 +0100

    fix: HostContext can also return Bamboo context

--- packages/components/src/Contexts/HostContext/index.tsx
+++ packages/components/src/Contexts/HostContext/index.tsx
@@ -1,3 +1,3 @@
 import { createContext } from 'react';
 
-export const HostContext = createContext<AP.JiraContext|AP.ConfluenceContext|undefined>(undefined);
+export const HostContext = createContext<AP.JiraContext|AP.ConfluenceContext|AP.BambooContext|undefined>(undefined);
