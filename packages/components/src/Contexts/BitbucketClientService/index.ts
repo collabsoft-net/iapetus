9600ffaa777e1143edc6f262276706fd8f69b2b3
Remie Bolte <remie@collabsoft.net>
2025-03-25 22:26:41 +0100

    fix: use conditional types to ensure typesafety between Cloud & Data Center

--- packages/components/src/Contexts/BitbucketClientService/index.ts
+++ packages/components/src/Contexts/BitbucketClientService/index.ts
@@ -1,6 +1,7 @@
 
+import { Modes } from '@collabsoft-net/enums';
 import kernel from '@collabsoft-net/inversify';
 import { BitbucketClientService as Service } from '@collabsoft-net/services';
 import React from 'react';
 
-export const BitbucketClientService = React.createContext<Service|null>(kernel.isBound(Service.getIdentifier()) ? kernel.get<Service>(Service.getIdentifier()) : null);
+export const BitbucketClientService = React.createContext<Service<Modes>|null>(kernel.isBound(Service.getIdentifier()) ? kernel.get<Service<Modes>>(Service.getIdentifier()) : null);
