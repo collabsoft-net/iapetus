9f8f30b2c990e6f62e3076f0510afb36f4019531
Remie Bolte <remie@collabsoft.net>
2025-05-10 18:15:09 +0200

    feat: add new Link component

--- packages/components/src/Hooks/useACJS.ts
+++ packages/components/src/Hooks/useACJS.ts
@@ -1,5 +1,7 @@
-import { AP, useContext } from '@collabsoft-net/components';
 import { ServiceIdentifier } from '@collabsoft-net/connect';
+
+import { AP } from '../Contexts/AP';
+import { useContext } from './useContext';
 
 export const useACJS = <T extends AP.JiraInstance|AP.ConfluenceInstance|AP.BambooInstance|AP.BitbucketInstance> () => {
     const ACJS = useContext<AP.JiraInstance|AP.ConfluenceInstance|AP.BambooInstance|AP.BitbucketInstance>(AP, ServiceIdentifier.AP);
