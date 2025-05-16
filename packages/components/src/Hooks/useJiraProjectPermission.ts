f97406a24318940e8271ac3f49ae0013af699763
Remie Bolte <remie@collabsoft.net>
2025-05-16 14:23:58 +0200

    fix: accountId should be optional for product specific requests

--- packages/components/src/Hooks/useJiraProjectPermission.ts
+++ packages/components/src/Hooks/useJiraProjectPermission.ts
@@ -1,26 +1,19 @@
-import { Modes } from '@collabsoft-net/enums';
 import { isOfType } from '@collabsoft-net/helpers';
-import { JiraClientService } from '@collabsoft-net/services';
-import { useQuery } from '@tanstack/react-query';
 
-import { useProductClientService } from './useProductClientService';
+import { useCurrentAccountId } from './useCurrentAccountId';
+import { useEntityPermission } from './useEntityPermission';
 
-export function useJiraProjectPermissions(project: Jira.Project, permissions: string|Array<string>, accountId: string, mode?: 'ALL'|'ANY'): [ boolean|undefined, boolean, Error|null ];
-export function useJiraProjectPermissions(projectId: number, permissions: string|Array<string>, accountId: string, mode?: 'ALL'|'ANY'): [ boolean|undefined, boolean, Error|null ];
-export function useJiraProjectPermissions(projects: Array<number>, permissions: string|Array<string>, accountId: string, mode?: 'ALL'|'ANY'): [ boolean|undefined, boolean, Error|null ];
-export function useJiraProjectPermissions(projectOrIdOrIds: Jira.Project|number|Array<number>, permissions: string|Array<string>, accountId: string, mode: 'ALL'|'ANY' = 'ALL'): [ boolean|undefined, boolean, Error|null ] {
-  const service = useProductClientService<JiraClientService<Modes>>();
-
-  const requiredPermissions = Array.isArray(permissions) ? permissions : [ permissions ];
+export function useJiraProjectPermissions(project: Jira.Project, permissions: string|Array<string>, accountId?: string, mode?: 'ALL'|'ANY'): [ boolean|undefined, boolean, Error|null ];
+export function useJiraProjectPermissions(projectId: number, permissions: string|Array<string>, accountId?: string, mode?: 'ALL'|'ANY'): [ boolean|undefined, boolean, Error|null ];
+export function useJiraProjectPermissions(projects: Array<number>, permissions: string|Array<string>, accountId?: string, mode?: 'ALL'|'ANY'): [ boolean|undefined, boolean, Error|null ];
+export function useJiraProjectPermissions(projectOrIdOrIds: Jira.Project|number|Array<number>, permissions: string|Array<string>, accountId?: string, mode: 'ALL'|'ANY' = 'ALL'): [ boolean|undefined, boolean, Error|null ] {
   const projects: Array<number> = isOfType<Jira.Project>(projectOrIdOrIds, 'id') ? [ Number(projectOrIdOrIds.id) ] : typeof projectOrIdOrIds === 'number' ? [ projectOrIdOrIds ] : projectOrIdOrIds;
 
-  const { data: permitted, isLoading: isLoadingPermissions, isFetching: isFetchingPermissions, error: permissionsError } = useQuery<boolean|undefined, Error>({
-    queryKey: [ 'JiraClientService.hasPermissions', accountId, requiredPermissions.join(','), mode ],
-    queryFn: () => service.hasPermissions(accountId, [ { projects, permissions: requiredPermissions }], undefined, mode).catch(() => false)
-  });
+  const [ atlassianAccountId, isLoadingAccountId ] = accountId
+    ? [ accountId, false ]
+    : useCurrentAccountId();
 
-  const isLoading = isLoadingPermissions || isFetchingPermissions;
-  const error = permissionsError;
-
-  return [ permitted, isLoading, error ];
+  return (!isLoadingAccountId && atlassianAccountId)
+    ? useEntityPermission('project', permissions, projects, atlassianAccountId, mode)
+    : [ undefined, true, null ];
 }
--- packages/components/src/Hooks/useJiraUser.ts
+++ packages/components/src/Hooks/useJiraUser.ts
@@ -1,7 +1,7 @@
 import { useCurrentUser } from './useCurrentUser';
 import { useUser } from './useUser';
 
-export const useJiraUser = (accountId?: string, expiresInSeconds?: number): [ Jira.User|undefined, boolean, Error|null ] => {
-  const [ user, loading, error ] = accountId ? useUser<Jira.User>(accountId, expiresInSeconds) : useCurrentUser<Jira.User>(expiresInSeconds);
-  return [ user, loading, error ];
-}
+export const useJiraUser = (accountId?: string, expiresInSeconds?: number): [ Jira.User|undefined, boolean, Error|null ] =>
+  accountId
+    ? useUser<Jira.User>(accountId, expiresInSeconds)
+    : useCurrentUser<Jira.User>(expiresInSeconds);
