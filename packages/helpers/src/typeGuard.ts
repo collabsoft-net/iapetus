3f17663bcffe7d90af72e10e31e212ced4009583
Remie Bolte <remie@collabsoft.net>
2025-05-28 17:07:24 +0200

    fix: add ability to infer type from property value

--- packages/helpers/src/typeGuard.ts
+++ packages/helpers/src/typeGuard.ts
@@ -1,9 +1,13 @@
 import { Type } from '@collabsoft-net/types';
 
-export const isOfType = <T>(obj: T | unknown, property: keyof T): obj is T => {
-    return obj !== null && typeof obj === 'object' && property in (obj as Record<string, unknown>);
+export function isOfType <T>(obj: T | unknown, property: keyof T): obj is T;
+export function isOfType <T>(obj: T | unknown, property: keyof T, value: unknown): obj is T;
+export function isOfType <T>(obj: T | unknown, property: keyof T, value?: unknown): obj is T {
+  return !value
+    ? obj !== null && typeof obj === 'object' && property in (obj as Record<string, unknown>)
+    : obj !== null && typeof obj === 'object' && property in (obj as Record<string, unknown>) && (obj as Record<keyof T, unknown>)[property] === value;
 };
 
 export const isTypeOf = <T, X>(instanceOrType: Type<T>|T, type: Type<X>): boolean => {
-    return (instanceOrType instanceof type || '' + instanceOrType === '' + type);
-  }
+  return (instanceOrType instanceof type || '' + instanceOrType === '' + type);
+}
