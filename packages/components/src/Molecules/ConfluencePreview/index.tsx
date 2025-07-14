1f101ca4740a20f5c55daf4b51523fa320dfe943
Remie Bolte <remie@collabsoft.net>
2025-07-14 10:46:49 +0200

    fix: make sure to set the correct font size / line height for SVGs

--- packages/components/src/Molecules/ConfluencePreview/index.tsx
+++ packages/components/src/Molecules/ConfluencePreview/index.tsx
@@ -95,7 +95,7 @@ export const ConfluencePreview = ({ showDisplayToolbar, defaultPageSize, childre
       <Grid fluid stretched border={ `1px solid ${token('color.border', colors.N40)}` } borderRadius='4px' background={ token('elevation.surface', colors.N0) }>
         <Row>
           <Grid fluid vertical padding='8px' boxShadow={ `0px 1px 1px 0px ${token('color.border', 'rgba(0, 0, 0, 0.10)')}` }>
-            <Column style={{ fontSize: 0 }}>
+            <Column style={{ fontSize: '16px', lineHeight: '16px' }}>
               <svg width="81" height="16" viewBox="0 0 81 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M2.25556 11.2197C2.12556 11.4317 1.97956 11.6777 1.85556 11.8737C1.80221 11.9638 1.78636 12.0713 1.81142 12.173C1.83647 12.2748 1.90044 12.3626 1.98956 12.4177L4.58956 14.0177C4.63474 14.0456 4.68503 14.0642 4.73748 14.0724C4.78993 14.0807 4.8435 14.0784 4.89507 14.0658C4.94663 14.0531 4.99516 14.0303 5.03783 13.9987C5.08049 13.9671 5.11644 13.9273 5.14356 13.8817C5.24756 13.7077 5.38156 13.4817 5.52756 13.2397C6.55756 11.5397 7.59356 11.7477 9.46156 12.6397L12.0396 13.8657C12.0879 13.8887 12.1403 13.9017 12.1938 13.9041C12.2473 13.9065 12.3007 13.8981 12.3509 13.8794C12.401 13.8608 12.4469 13.8322 12.4859 13.7955C12.5248 13.7588 12.556 13.7146 12.5776 13.6657L13.8156 10.8657C13.8576 10.7695 13.8602 10.6608 13.8228 10.5627C13.7854 10.4647 13.711 10.3853 13.6156 10.3417C13.0716 10.0857 11.9896 9.57566 11.0156 9.10566C7.51156 7.40366 4.53356 7.51366 2.25556 11.2197Z" fill="url(#paint0_linear_17161_33435)"/>
                 <path d="M13.9135 4.86623C14.0435 4.65423 14.1895 4.40823 14.3135 4.21223C14.3669 4.12207 14.3827 4.01458 14.3577 3.91286C14.3326 3.81113 14.2687 3.7233 14.1795 3.66823L11.5795 2.06823C11.534 2.03758 11.4827 2.01661 11.4287 2.00665C11.3747 1.99668 11.3193 1.99793 11.2658 2.01031C11.2124 2.02269 11.162 2.04594 11.1179 2.07862C11.0738 2.11129 11.0369 2.15269 11.0095 2.20023C10.9055 2.37423 10.7715 2.60023 10.6255 2.84223C9.59553 4.54223 8.55953 4.33423 6.69153 3.44223L4.12153 2.22223C4.0732 2.19923 4.02075 2.18616 3.96728 2.1838C3.91381 2.18144 3.86041 2.18983 3.81024 2.20848C3.76008 2.22713 3.71416 2.25566 3.67522 2.29237C3.63628 2.32909 3.6051 2.37325 3.58353 2.42223L2.34553 5.22223C2.30349 5.31835 2.3009 5.42714 2.33831 5.52516C2.37572 5.62318 2.45014 5.70257 2.54553 5.74623C3.08953 6.00223 4.17153 6.51223 5.14553 6.98223C8.65753 8.68223 11.6355 8.56823 13.9135 4.86623Z" fill="url(#paint1_linear_17161_33435)"/>
@@ -112,7 +112,7 @@ export const ConfluencePreview = ({ showDisplayToolbar, defaultPageSize, childre
                 </defs>
               </svg>
             </Column>
-            <OptionalNavColumn style={{ fontSize: 0 }} marginLeft="24px">
+            <OptionalNavColumn style={{ fontSize: '16px', lineHeight: '16px' }} marginLeft="24px">
               <svg width="122" height="16" viewBox="0 0 122 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="0" y="5" width="32" height="6" rx="3" fill={ token('color.skeleton', colors.N30) } />
                 <rect x="40" y="5" width="32" height="6" rx="3" fill={ token('color.skeleton', colors.N30) } />
@@ -121,7 +121,7 @@ export const ConfluencePreview = ({ showDisplayToolbar, defaultPageSize, childre
               </svg>
             </OptionalNavColumn>
             <Column stretched></Column>
-            <Column style={{ fontSize: 0 }}>
+            <Column style={{ fontSize: '16px', lineHeight: '16px' }}>
               <svg width="148" height="16" viewBox="0 0 148 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <rect x="0.5" y="0.5" width="63" height="15" rx="1.5" fill={ token('color.skeleton', colors.N30) }/>
                 <rect x="72" y="2" width="12" height="12" rx="6" fill={ token('color.skeleton', colors.N30) } />
@@ -199,12 +199,12 @@ export const ConfluencePreview = ({ showDisplayToolbar, defaultPageSize, childre
       { (showDisplayToolbar !== false) && (
         <PreviewWidth>
           <Grid fluid vertical gap='8px' padding='4px 8px' borderRadius='4px' border={ `${token('border.width', '1px')} solid ${token('color.border', colors.N300A)}`} background={ token('elevation.surface.raised', colors.N0) }>
-            <Column onClick={ () => setPageSize('full-width') } background={ pageSize === 'full-width' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer'>
+            <Column onClick={ () => setPageSize('full-width') } background={ pageSize === 'full-width' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer' style={{ fontSize: '24px', lineHeight: '24px' }}>
               <Tooltip content='Make page full-width'>
                 <EditorMediaExpandIcon label='Full width' primaryColor={ pageSize === 'full-width' ? token('color.icon.information', colors.B400) : token('color.icon') } />
               </Tooltip>
             </Column>
-            <Column onClick={ () => setPageSize('fixed-width') } background={ pageSize === 'fixed-width' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer'>
+            <Column onClick={ () => setPageSize('fixed-width') } background={ pageSize === 'fixed-width' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer' style={{ fontSize: '24px', lineHeight: '24px' }}>
               <Tooltip content='Make page fixed-width'>
                 <EditorMediaCollapseIcon label='Fixed width' primaryColor={ pageSize === 'fixed-width' ? token('color.icon.information', colors.B400) : token('color.icon') } />
               </Tooltip>
@@ -212,17 +212,17 @@ export const ConfluencePreview = ({ showDisplayToolbar, defaultPageSize, childre
             { pageSize === 'fixed-width' && (
               <>
                 <Column align='center'><Divider /></Column>
-                <Column onClick={ () => setMacroSize('center') } background={ macroSize === 'center' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer'>
+                <Column onClick={ () => setMacroSize('center') } background={ macroSize === 'center' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer' style={{ fontSize: '24px', lineHeight: '24px' }}>
                   <Tooltip content='Back to center'>
                     <EditorMediaCenterIcon label='Back to center' primaryColor={ macroSize === 'center' ? token('color.icon.information', colors.B400) : token('color.icon') } />
                   </Tooltip>
                 </Column>
-                <Column onClick={ () => setMacroSize('wide') } background={ macroSize === 'wide' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer'>
+                <Column onClick={ () => setMacroSize('wide') } background={ macroSize === 'wide' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer' style={{ fontSize: '24px', lineHeight: '24px' }}>
                   <Tooltip content='Go wide'>
                     <EditorMediaWideIcon label='Go wide' primaryColor={ macroSize === 'wide' ? token('color.icon.information', colors.B400) : token('color.icon') } />
                   </Tooltip>
                 </Column>
-                <Column onClick={ () => setMacroSize('full-width') } background={ macroSize === 'full-width' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer'>
+                <Column onClick={ () => setMacroSize('full-width') } background={ macroSize === 'full-width' ? token('color.background.information', colors.B50) : undefined } borderRadius='2px' cursor='pointer' style={{ fontSize: '24px', lineHeight: '24px' }}>
                   <Tooltip content='Go full width'>
                     <EditorMediaFullWidthIcon label='Go full width' primaryColor={ macroSize === 'full-width' ? token('color.icon.information', colors.B400) : token('color.icon') } />
                   </Tooltip>
