cb5d660173db288f4330b809e5dacba9d4fe11ca
Remie Bolte <remie@collabsoft.net>
2025-10-04 08:45:09 +0200

    fix: provide de context as-is

--- packages/components/src/Contexts/PlatformBridge/index.ts
+++ packages/components/src/Contexts/PlatformBridge/index.ts
@@ -5,8 +5,8 @@ import { ServiceIdentifier } from '@collabsoft-net/platform';
 import { AbstractRestClientService } from '@collabsoft-net/services';
 import React from 'react';
 
-export const PlatformBridge = <T extends Applications, X extends AbstractRestClientService> () => React.createContext<Platform.Bridge<T, X>|null>(
+export const PlatformBridge = React.createContext<Platform.Bridge<Applications, AbstractRestClientService>|null>(
   kernel.isBound(ServiceIdentifier.PlatformBridge)
-    ? kernel.get<Platform.Bridge<T, X>>(ServiceIdentifier.PlatformBridge)
+    ? kernel.get(ServiceIdentifier.PlatformBridge)
     : null
 );
--- packages/components/src/Hooks/usePlatformBridge.ts
+++ packages/components/src/Hooks/usePlatformBridge.ts
@@ -7,9 +7,9 @@ import { PlatformBridge as context } from '../Contexts/PlatformBridge';
 import { useContext } from './useContext';
 
 export const usePlatformBridge = <T extends Applications, X extends AbstractRestClientService> (): Platform.Bridge<T, X>  => {
-  const bridge = useContext<Platform.Bridge<T, X>>(context(), ServiceIdentifier.PlatformBridge)
+  const bridge = useContext(context, ServiceIdentifier.PlatformBridge)
   if (bridge) {
-    return bridge;
+    return bridge as Platform.Bridge<T, X>;
   }
 
   throw new Error(`Unable to detect Platform bridge. Please make sure to initialize it and bind it to ${ServiceIdentifier.PlatformBridge.toString()}`)
