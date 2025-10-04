91906c2f713838e20ebb09e216d94e00ee5d9365
Remie Bolte <remie@collabsoft.net>
2025-10-04 07:32:38 +0200

    fix: improve type safety

--- packages/components/src/Contexts/PlatformBridge/index.ts
+++ packages/components/src/Contexts/PlatformBridge/index.ts
@@ -2,10 +2,11 @@
 import { Applications } from '@collabsoft-net/enums';
 import kernel from '@collabsoft-net/inversify';
 import { ServiceIdentifier } from '@collabsoft-net/platform';
+import { AbstractRestClientService } from '@collabsoft-net/services';
 import React from 'react';
 
-export const PlatformBridge = React.createContext<Platform.Bridge<Applications>|null>(
+export const PlatformBridge = <T extends Applications, X extends AbstractRestClientService> () => React.createContext<Platform.Bridge<T, X>|null>(
   kernel.isBound(ServiceIdentifier.PlatformBridge)
-    ? kernel.get<Platform.Bridge<Applications>>(ServiceIdentifier.PlatformBridge)
+    ? kernel.get<Platform.Bridge<T, X>>(ServiceIdentifier.PlatformBridge)
     : null
 );
--- packages/components/src/Hooks/usePlatformBridge.ts
+++ packages/components/src/Hooks/usePlatformBridge.ts
@@ -1,14 +1,15 @@
 
 import { Applications } from '@collabsoft-net/enums';
 import { ServiceIdentifier } from '@collabsoft-net/platform';
+import { AbstractRestClientService } from '@collabsoft-net/services';
 
 import { PlatformBridge as context } from '../Contexts/PlatformBridge';
 import { useContext } from './useContext';
 
-export const usePlatformBridge = (): Platform.Bridge<Applications>  => {
-  const bridge = useContext(context, ServiceIdentifier.PlatformBridge)
+export const usePlatformBridge = <T extends Applications, X extends AbstractRestClientService> (): Platform.Bridge<T, X>  => {
+  const bridge = useContext<Platform.Bridge<T, X>>(context(), ServiceIdentifier.PlatformBridge)
   if (bridge) {
-    return bridge
+    return bridge;
   }
 
   throw new Error(`Unable to detect Platform bridge. Please make sure to initialize it and bind it to ${ServiceIdentifier.PlatformBridge.toString()}`)
