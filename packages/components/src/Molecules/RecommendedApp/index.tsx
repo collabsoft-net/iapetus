4322bb765d3ff8368309a704512e72af6984d52b
Remie Bolte <remie@collabsoft.net>
2026-06-07 08:30:23 +0200

    fix: use platform-aware link

--- packages/components/src/Molecules/RecommendedApp/index.tsx
+++ packages/components/src/Molecules/RecommendedApp/index.tsx
@@ -1,9 +1,9 @@
 import Avatar from '@atlaskit/avatar';
-import Link from '@atlaskit/link';
 import styled from '@emotion/styled';
 import React from 'react';
 
 import { Column, Grid } from '../../Atoms/Grid';
+import { Link } from '../Link';
 
 interface RecommendedAppProps {
   logo: string;
@@ -20,7 +20,7 @@ export const RecommendedApp = ({ logo, name, url }: RecommendedAppProps): JSX.El
         <Avatar size='medium' appearance='square' src={ logo } />
       </ImageWrapper>
       <Column stretched align='center'>
-        <Link href={url} target="_blank" rel="noreferrer">{ name }</Link>
+        <Link type="external" href={url}>{ name }</Link>
       </Column>
     </Grid>
   );
